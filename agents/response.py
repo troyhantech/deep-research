30cabe646d9d2592a9f43ce892881287af150747
troyhan <22350747@qq.com>
2025-08-18 14:19:03 +0800

    fix(agents/response): update incomplete-tool tips

--- agents/response.py
+++ agents/response.py
@@ -1,7 +1,7 @@
 class Response:
     noToolUse = "[Error] You did not use any tool in previous response! Please retry with a tool use."
 
-    partialToolUse = "[Error] The tool use is not complete! Please retry with a complete tool use. Note: Maybe the output content too long, causing the output to be truncated."
+    partialToolUse = "[Error] The tool use is not complete! Please check previous response and retry with a complete tool use. Possible reasons: 1. Ensure the tool format is correct(tool_name, params, and tag close). 2. Maybe the output content too long, causing the output to be truncated."
     noMoreReasoningTimes = "[URGENT AND MANDATORY] Only one tool call opportunity remains! Must generate the report based on the available information and deliver it directly."
 
     @staticmethod
